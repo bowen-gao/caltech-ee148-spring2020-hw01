42eda9efd4e40587879e5192c3757a40614d7d83
Bowen Gao <billgao0111@gmail.com>
2020-04-14 18:25:59 -0700

    add potential problem

--- q4q5q6.py
+++ q4q5q6.py
@@ -5,7 +5,7 @@ f = open('hw01_preds/preds.json')
 
 preds = json.load(f)
 
-# code for question 5
+# code for question 4
 
 # here are some images that my algorithm succeeded
 good_images = ["RL-036.jpg", "RL-048.jpg", "RL-332.jpg"]
@@ -16,6 +16,8 @@ for image in good_images:
     for box in preds[image]:
         draw.rectangle([box[1], box[0], box[3], box[2]])
     I.show()
+
+# code for question 5
 
 # here are some images that my algorithm failed
 bad_images = ["RL-334.jpg", "RL-167.jpg", "RL-163.jpg"]
